8e63b00b8af8ccee0bf4a4eaead279f25acb46df
Abhinav Kumar <work.abhinavkr@gmail.com>
2021-02-17 22:18:37 +0530

    added pagination

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const cheerio = require('cheerio');
-const got = require('got');
+const axios = require('axios');
+const qs = require('qs');
 
 const BASE_URL = "https://www.heritageit.edu/";
 const NOTICE_URL = "https://www.heritageit.edu/Notice.aspx";
 
 //matches if the given relative url is valid url of notice pdf file or not
-const matchNoticeUrl = val => /NoticePDF\/[0-9]+NOT[0-9]+.pdf/i.test(val)
+//const matchNoticeUrl = val => /NoticePDF\/[0-9]+NOT[0-9]+.pdf/i.test(val) || /NoticePDF\/[0-9]+Notice[0-9]+.pdf/i.test(val) 
+const matchNoticeUrl = val => /NoticePDF\/.+\.pdf/i.test(val);
 
-//Reads data from the heritage website
-const getData = async () => {
+const getId = (relUrl) => relUrl.toUpperCase().replace("NOTICEPDF/","").replace(".PDF","");
 
-    //creates a get request and returns the response
-    const response = await got(NOTICE_URL);
-    
+const parseNotices = (body) => {
     //cheerio parses the html and returns a jquery object 
     //to traverse and manipulate the DOM.
-    const $ = cheerio.load(response.body);
+    const $ = cheerio.load(body);
 
     //This query returns list of all the <tr> child of <table> 
     //with id ctl00_ContentPlaceHolder1_GridView1
     const trows = $('table#ctl00_ContentPlaceHolder1_GridView1 tr').toArray();
+    
     const notices = [];
     
     for(let i = 0,tr,name,date; i < trows.length; i++){
@@ -39,6 +39,7 @@ const getData = async () => {
         //Then, this tr does not wrap a notice data
         if(matchNoticeUrl(relUrl)){
             notices.push({
+                id: getId(relUrl),
                 url: BASE_URL+relUrl,
                 relUrl,
                 name,
@@ -46,7 +47,47 @@ const getData = async () => {
             });
         }
     }
-    return notices;    
+    return notices;
+}
+const extractFormData = (body,page) => {
+    const $ = cheerio.load(body);
+    const __VIEWSTATE = $("#__VIEWSTATE").attr("value");
+    const __EVENTVALIDATION = $("#__EVENTVALIDATION").attr("value");
+    const __VIEWSTATEGENERATOR = $("#__VIEWSTATEGENERATOR").attr("value");
+    return {
+        __EVENTTARGET: "ctl00$ContentPlaceHolder1$GridView1",
+        __EVENTARGUMENT: "Page$"+page,
+        __VIEWSTATE,
+        __VIEWSTATEGENERATOR,
+        __EVENTVALIDATION
+    }
+}
+//Reads data from the heritage website
+const getData = async (page=1) => {
+
+    //creates a get request and returns the response
+    //const response = await got(NOTICE_URL);
+    var config = {
+        method: 'get',
+        url: NOTICE_URL
+    };
+    const response = await axios(config);
+    if(page===1 || page === "1")
+        return parseNotices(response.data);
+    else {
+        var formData = extractFormData(response.data,page);
+        var data = qs.stringify(formData);
+        var config2 = {
+            method: "post",
+            url: NOTICE_URL,
+            headers: { 
+                'Content-Type': 'application/x-www-form-urlencoded', 
+            },
+            data : data
+        };
+        const response2 = await axios(config2);
+        return parseNotices(response2.data);
+    }
 }
 
 exports.getData = getData;
@@ -54,8 +95,9 @@ exports.getData = getData;
 //cloud function
 exports.getNotices = async (req,res) => {
     const isPretty = req.query.pretty == 1;
+    const page = parseInt(req.query.page);
     try {
-        const notices = await getData();    
+        const notices = await getData(page);    
         if(isPretty){
             res.set('Content-Type','text/html');
             res.send(
--- scrap.js
+++ scrap.js
@@ -1,6 +1,5 @@
 const { getData } = require("./index");
-
-getData()
+getData(process.argv.length > 2 ? parseInt(process.argv[2]) : 1)
 .then(data=>{
     console.log(data);
 })
