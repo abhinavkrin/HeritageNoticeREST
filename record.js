a79afc9c085f407ceb1b8c04072c357a4f7951bc
Abhinav Kumar <work.abhinavkr@gmail.com>
2021-02-19 17:04:22 +0530

    record.js

--- record.js
+++ record.js
@@ -1,19 +1,7 @@
 require('dotenv').config();
 const { recordNotices } = require("./common");
 
-// recordNotices(process.argv.length > 2 ? parseInt(process.argv[2]) : 1,false)
-// .then(()=>{
-//     console.log("Recorded. OK!");
-// })
-
-
-(async function(){
-    for(i=25;i>=1;i--){
-        try {
-            await recordNotices(i,true)
-            console.log("Recorded Page ",i);
-        } catch(e){
-            console.error(e);
-        }
-    }
-})();
+recordNotices(process.argv.length > 2 ? parseInt(process.argv[2]) : 1,false)
+.then(()=>{
+    console.log("Recorded. OK!");
+})
